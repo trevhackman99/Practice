211df23fc8c38aae0d8e4745c285c54618192d4b
trevhackman99 <115102537+trevhackman99@users.noreply.github.com>
2024-06-18 05:47:37 -0700

    Progress in home.html & script.js
    script.js now creates divs for each object (card) returned by API fetch and displays an img and some general info from the JSON supplied

--- api-practice/pokemon-tcg/script.js
+++ api-practice/pokemon-tcg/script.js
@@ -9,7 +9,7 @@ async function fetchData() {
 
     // API URL and TOKEN
     const url = 'https://api.pokemontcg.io/v2';
-    const TOKEN = 'ba029d49-2600-4cca-9a3d-0167a4b17167';
+    const TOKEN = 'xxx';
 
     // TRY / CATCH block to handle request and report errors
     try {
@@ -23,9 +23,9 @@ async function fetchData() {
 
         if (response.ok) {
             const data = await response.json();
-            const card = data.data[0];
-            console.log('Success:', card.images.small);
-            showImg(card.images.small);
+            const cards = data.data;
+            console.log('Success:', cards);
+            createDiv(cards);
         } else {
             console.error('Error:', response.status);
         }
@@ -34,10 +34,73 @@ async function fetchData() {
     }
 }
 
-function showImg(imgUrl) {
-    const img = document.createElement('img');
-    img.src = imgUrl;
-    document.body.appendChild(img);
+function createDiv(cards) {
+    
+    const container 
+    = document.getElementById('search-res-ctn');
+
+    cards.forEach((card) => {
+        const cardCtn =
+        document.createElement('div');
+        cardCtn.setAttribute('class', 'card-ctn');
+        container.appendChild(cardCtn);
+        
+        const cardImgCtn =
+        document.createElement('div');
+        cardImgCtn.setAttribute('class', 'card-img');
+        container.appendChild(cardImgCtn);
+
+        const cardImg =
+        document.createElement('img');
+        cardImg.setAttribute('src', card.images.small);
+        cardCtn.appendChild(cardImg);
+
+        const cardInfoCtn =
+        document.createElement('div');
+        cardInfoCtn.setAttribute('class', 'card-info');
+        cardCtn.appendChild(cardInfoCtn);
+
+        const cardName =
+        document.createElement('h3');
+        cardName.setAttribute('class', 'card-name');
+        cardName.textContent = card.name;
+        cardInfoCtn.appendChild(cardName);
+
+        const cardSet =
+        document.createElement('p');
+        cardSet.setAttribute('class', 'card-set');
+        cardSet.textContent = card.set.name;
+        cardInfoCtn.appendChild(cardSet);
+
+        const cardNum =
+        document.createElement('p');
+        cardNum.setAttribute('class', 'card-num');
+        cardNum.textContent = card.number;
+        cardInfoCtn.appendChild(cardNum);
+
+        const cardPriceMkt =
+        document.createElement('p');
+        cardPriceMkt.setAttribute('class', 'card-price-mkt');
+        
+        const cardPrices =
+        card.tcgplayer.prices;
+        // console.log(cardPrices);
+
+        for (print in cardPrices) {
+
+            const cardPrint =
+            document.createElement('p');
+            cardPrint.setAttribute('class', 'card-price');
+            
+            cardPrint.textContent = `${print}: ${cardPrices[print].market}`;
+            cardPriceMkt.appendChild(cardPrint);
+            };
+
+        cardInfoCtn.appendChild(cardPriceMkt);
+
+
+
+    });
 }
 
     
