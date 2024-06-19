545a8533d36a443ecea23e67acd53c5ff707b2a6
trevhackman99 <115102537+trevhackman99@users.noreply.github.com>
2024-06-19 06:15:42 -0700

    CSS styling, html and script corrections

--- api-practice/pokemon-tcg/script.js
+++ api-practice/pokemon-tcg/script.js
@@ -9,7 +9,7 @@ async function fetchData() {
 
     // API URL and TOKEN
     const url = 'https://api.pokemontcg.io/v2';
-    const TOKEN = 'xxx';
+    const TOKEN = '';
 
     // TRY / CATCH block to handle request and report errors
     try {
@@ -40,42 +40,52 @@ function createDiv(cards) {
     = document.getElementById('search-res-ctn');
 
     cards.forEach((card) => {
+
+        // Creates 'card-ctn' div and appends to 'search-res-ctn
         const cardCtn =
         document.createElement('div');
+
         cardCtn.setAttribute('class', 'card-ctn');
         container.appendChild(cardCtn);
         
+        // 
         const cardImgCtn =
         document.createElement('div');
-        cardImgCtn.setAttribute('class', 'card-img');
-        container.appendChild(cardImgCtn);
+
+        cardImgCtn.setAttribute('class', 'card-img-ctn');
+        cardCtn.appendChild(cardImgCtn);
 
         const cardImg =
         document.createElement('img');
+
         cardImg.setAttribute('src', card.images.small);
-        cardCtn.appendChild(cardImg);
+        cardImgCtn.appendChild(cardImg);
 
         const cardInfoCtn =
         document.createElement('div');
-        cardInfoCtn.setAttribute('class', 'card-info');
+
+        cardInfoCtn.setAttribute('class', 'card-info-ctn');
         cardCtn.appendChild(cardInfoCtn);
 
         const cardName =
         document.createElement('h3');
+
         cardName.setAttribute('class', 'card-name');
         cardName.textContent = card.name;
         cardInfoCtn.appendChild(cardName);
 
         const cardSet =
         document.createElement('p');
+
         cardSet.setAttribute('class', 'card-set');
         cardSet.textContent = card.set.name;
         cardInfoCtn.appendChild(cardSet);
 
         const cardNum =
         document.createElement('p');
+
         cardNum.setAttribute('class', 'card-num');
-        cardNum.textContent = card.number;
+        cardNum.textContent = 'Card Number: ' + card.number;
         cardInfoCtn.appendChild(cardNum);
 
         const cardPriceMkt =
@@ -92,7 +102,14 @@ function createDiv(cards) {
             document.createElement('p');
             cardPrint.setAttribute('class', 'card-price');
             
-            cardPrint.textContent = `${print}: ${cardPrices[print].market}`;
+            if (print === 'reverseHolofoil') {
+                cardPrint.textContent = `Reverse Holofoil: ${cardPrices[print].market}`;
+            } else if (print === 'holofoil') {
+                cardPrint.textContent = `Holofoil: ${cardPrices[print].market}`;
+            } else {
+                cardPrint.textContent = `Normal: ${cardPrices[print].market}`;
+            }
+
             cardPriceMkt.appendChild(cardPrint);
             };
 
