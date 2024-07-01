30a816146cc0d2f0caee7ace8060d4803a3d0184
trevhackman99 <115102537+trevhackman99@users.noreply.github.com>
2024-07-01 09:00:14 -0700

    node server created, script fetches, and html table

---
+++ script.js
@@ -0,0 +1,39 @@
+window.onload = function() {
+    fetch('formattedData.json')
+        .then(response => response.json())
+        .then(data => {
+            
+            // console.log(`${data[0].name} - ${data[0].version}`)
+            for (let i = 0; i < data.length; i++) {
+                const table = document.getElementById('mainTable');
+                let row = document.createElement('tr');
+                let cell = document.createElement('td');
+                cell.textContent = `${data[i].name} - ${data[i].version}`;
+                row.appendChild(cell);
+                cell = document.createElement('td');
+                cell.textContent = data[i].set;
+                row.appendChild(cell);
+                cell = document.createElement('td');
+                cell.textContent = data[i].collector_number;
+                row.appendChild(cell);
+                cell = document.createElement('td');
+                cell.textContent = data[i].rarity;
+                row.appendChild(cell);
+                cell = document.createElement('td');
+                cell.textContent = data[i].normal;
+                row.appendChild(cell);
+                cell = document.createElement('td');
+                cell.textContent = data[i].foil;
+                row.appendChild(cell);
+
+                table.appendChild(row);
+            }
+        }) 
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        }
+    );
+
+    
+};
+
