c6d80aca7b7ad43a35dad9c8afdd54abedf343de
trevhackman99 <115102537+trevhackman99@users.noreply.github.com>
2024-07-27 19:27:17 -0700

    Add files via upload

---
+++ api-practice/buylist-tool/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/buylist.html');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
