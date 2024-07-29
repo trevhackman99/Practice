b2414ce3190b3caecc9e50014b78180e68ee38a8
trevhackman99 <115102537+trevhackman99@users.noreply.github.com>
2024-07-29 13:00:53 -0700

    Add files via upload

---
+++ big-league-game/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import './game.css';
+import Game from './components/Game';
+
+function App() {
+
+  
+  
+  return (
+    <div className="App">
+      <Game />
+    </div>
+  );
+  
+}
+
+
+export default App;
